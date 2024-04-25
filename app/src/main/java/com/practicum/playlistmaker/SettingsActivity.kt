746b716cf766ebd264f20a905541167b30c7df6e
Selin-Eglence <sel.eglence@yandex.ru>
2024-04-25 12:53:34 +0300

    2

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,48 @@ class SettingsActivity : AppCompatActivity() {
 
     val arrow = findViewById<Button>(R.id.light_mode)
     arrow.setOnClickListener {
-        val intent = Intent(this@SettingsActivity, MainActivity::class.java)
-        startActivity(intent)
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+    val switch = findViewById<SwitchCompat>(R.id.switchBtn)
+    switch.setOnClickListener {
+        if (switch.isChecked) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        else {
+            AppCompatDelegate.setDefaultNightMode (AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+
     }
-}}
+
+
+        val share = findViewById<Button>(R.id.vector)
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message))
+            shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+        }
+
+
+
+        val support = findViewById<Button>(R.id.support)
+        support.setOnClickListener{
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.contact)))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.context))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.topic))
+            startActivity(supportIntent)
+        }
+
+        val agreement = findViewById<Button>(R.id.arrow_forward)
+        agreement.setOnClickListener {
+            val agreeIntent=Intent(Intent.ACTION_VIEW)
+            agreeIntent.data= Uri.parse(getString(R.string.agreement))
+            startActivity(agreeIntent)
+        }
+
+    }}
