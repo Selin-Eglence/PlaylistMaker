95a80e9c74a8ab2314e966adba26e976b0e267ef
Selin-Eglence <154987194+Selin-Eglence@users.noreply.github.com>
2024-04-25 13:36:37 +0300

    Revert "1"

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.practicum.playlistmaker
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val searchButton = findViewById<Button>(R.id.lupa1)
-        val searchButtonClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val intent = Intent(this@MainActivity, Poisk_Activity::class.java)
-                startActivity(intent)
-            }
-        }
-        searchButton.setOnClickListener(searchButtonClickListener)
-
-
-        val mediaButton = findViewById<Button>(R.id.music)
-        mediaButton.setOnClickListener {
-            val intent = Intent(this@MainActivity, Media_Activity::class.java)
-            startActivity(intent)
-        }
-
-        val settingsButton = findViewById<Button>(R.id.settings)
-        settingsButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-                startActivity(intent)
-        }})
     }
 }
